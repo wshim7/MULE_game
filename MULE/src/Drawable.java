1d80992b609a3885063511386eda1254de0c38d6
WongooShim <wgshim7@google.com>
2016-08-06 20:41:29 -0400

    import

---
+++ MULE/src/Drawable.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+
+/**
+ * The Drawable interface is implemented by any object in the game that needs to
+ * be drawn during gameplay. These objects must be able to draw themselves using
+ * the draw(Graphics) method in this interface.
+ * 
+ * @author Chris Jenkins (cjenkins36)
+ * 
+ */
+public interface Drawable {
+	public void draw(Graphics g);
+}
